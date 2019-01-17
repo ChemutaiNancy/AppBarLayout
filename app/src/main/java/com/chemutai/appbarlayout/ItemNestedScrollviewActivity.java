c6ad0dffa07f9977ffc18205c890566451cfeb25
Nancy <chemutain@gmail.com>
2019-01-17 11:46:20 +0300

    App Bar Layout

---
+++ app/src/main/java/com/chemutai/appbarlayout/ItemNestedScrollviewActivity.java
@@ -0,0 +1,13 @@
+package com.chemutai.appbarlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemNestedScrollviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_nested_scrollview);
+    }
+}
